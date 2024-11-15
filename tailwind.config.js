750b9bb2ce143d8d51be33a392d6ef37876b18ee
Mustain90 <mustainhossain.200@gmail.com>
2024-11-15 17:34:23 +0600

    all commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+          
+      },
+    },
+    plugins: [],
+
+}
+
+
+module.exports = {
+    theme: {
+      extend: {
+        backgroundImage: {
+          'hero-pattern': "url('projects/B9A3-Hockays-main/images/2.png')",
+          'footer-texture': "url('/img/footer-texture.png')",
+        }
+      }
+    }
+  }
+
+
